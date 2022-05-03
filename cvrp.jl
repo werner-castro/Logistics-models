bd49e1fefadce866a89534e23f7cb0854d81a4af
werner castro <63932214+werner-castro@users.noreply.github.com>
2022-05-03 10:35:10 -0300

    Delete cvrp.jl

--- cvrp.jl
+++
@@ -1,7 +0,0 @@
-
-# meta pacote para o problema de roteamento de veículos capacitado com heurísticas e modelos
-module Cvrp
-    include("heuristics.jl")
-    include("utils.jl")
-    using .Heuristic
-end # end module
